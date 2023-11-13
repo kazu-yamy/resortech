b09822fcfe54a2b2b701db871185b0d345c8521e
nakahira <nakahira@nakahira.com>
2023-11-13 15:33:52 +0900

    repush sorcecode

---
+++ src/backend/api/predict.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, UploadFile
+from utils.tensor import Tensor
+
+router = APIRouter(prefix="/api/v1", tags=["/api/v1"])
+
+
+@router.post("/predict")
+def predict(image: UploadFile):
+    tensor = Tensor(image.file.read())
+    result = tensor.predict_image()
+    print(result)
+    return {"result": result}
